2cbdebd9c839f3fe968e3da680d4e78a27faee4b
wangpeng12 <wangpeng12@kuaishou.com>
2021-09-02 20:42:33 +0800

    MOD bench.cpp to make actions happy

--- bench.cpp
+++ bench.cpp
@@ -8,7 +8,8 @@ static void fib_10(benchmark::State &state) {
     // Suppress optimization otherwise this line is removed by DCE
     int i = 10;
     int a = 20;
-    std::cout << "a:" << a << std::endl;
+    // std::cout << "a:" << a << std::endl;
+    benchmark::DoNotOptimize(a);
     benchmark::DoNotOptimize(i);
     benchmark::DoNotOptimize(fib(i));
   }
@@ -18,7 +19,8 @@ static void fib_20(benchmark::State &state) {
   for (auto _ : state) {
     int i = 20;
     int b = 50;
-    std::cout << "b:" << b << std::endl;
+    // std::cout << "b:" << b << std::endl;
+    benchmark::DoNotOptimize(b);
     benchmark::DoNotOptimize(i);
     benchmark::DoNotOptimize(fib(i));
   }
