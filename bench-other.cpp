7033cfbe6e59595cb13a476e6e9fc6ac00dab73e
wangpeng12 <wangpeng12@kuaishou.com>
2021-09-02 21:14:51 +0800

    MOD source code to make test happy

--- bench-other.cpp
+++ bench-other.cpp
@@ -3,10 +3,10 @@
 
 #include <iostream>
 
-static void fib_10(benchmark::State &state) {
+static void fib_30(benchmark::State &state) {
   for (auto _ : state) {
     // Suppress optimization otherwise this line is removed by DCE
-    int i = 10;
+    int i = 30;
     int a = 20;
     benchmark::DoNotOptimize(a);
     benchmark::DoNotOptimize(i);
@@ -14,9 +14,9 @@ static void fib_10(benchmark::State &state) {
   }
 }
 
-static void fib_20(benchmark::State &state) {
+static void fib_40(benchmark::State &state) {
   for (auto _ : state) {
-    int i = 20;
+    int i = 40;
     int b = 50;
     benchmark::DoNotOptimize(b);
     benchmark::DoNotOptimize(i);
@@ -25,8 +25,8 @@ static void fib_20(benchmark::State &state) {
 }
 
 // Register the function as a benchmark
-BENCHMARK(fib_10);
-BENCHMARK(fib_20);
+BENCHMARK(fib_30);
+BENCHMARK(fib_40);
 
 // Run the benchmark
 BENCHMARK_MAIN();
