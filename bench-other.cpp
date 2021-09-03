125bb8985ea7fcf5cd7b9983dfaa8581af73a107
wangpeng12 <wangpeng12@kuaishou.com>
2021-09-03 10:38:26 +0800

    MOD: mod bench-other.cpp to make test happy

--- bench-other.cpp
+++ bench-other.cpp
@@ -8,8 +8,8 @@ static void fib_30(benchmark::State &state) {
     // Suppress optimization otherwise this line is removed by DCE
     int i = 30;
     int a = 20;
-    benchmark::DoNotOptimize(a);
-    benchmark::DoNotOptimize(i);
+    // benchmark::DoNotOptimize(a);
+    // benchmark::DoNotOptimize(i);
     benchmark::DoNotOptimize(fib(i));
   }
   // state.counters["ops"] = benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
@@ -18,8 +18,8 @@ static void fib_30(benchmark::State &state) {
 static void fib_40(benchmark::State &state) {
   for (auto _ : state) {
     int i = 40;
-    int b = 50;
-    benchmark::DoNotOptimize(b);
+    // int b = 50;
+    // benchmark::DoNotOptimize(b);
     benchmark::DoNotOptimize(i);
     benchmark::DoNotOptimize(fib(i));
   }
@@ -30,7 +30,7 @@ static void fib_40(benchmark::State &state) {
 BENCHMARK(fib_30)->DenseThreadRange(1, 6, 2)->UseRealTime()->Repetitions(5)->ReportAggregatesOnly(true);
 BENCHMARK(fib_40)->DenseThreadRange(1, 6, 2)->UseRealTime()->Repetitions(5)->ReportAggregatesOnly(true);
 
-extern bool FLAGS_benchmark_counters_tabular;
+// extern bool FLAGS_benchmark_counters_tabular;
 // Run the benchmark
 BENCHMARK_MAIN();
 /*
